01253a98b04257264c7d90cbaec298e9e6a69193
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-07 16:54:26 +0200

    com0

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, send_file
 
-
+app=Flask(__name__)
 
 @app.route("/",methods=['GET'])
 def get_default():
     return jsonify({'result': "App Running..."})
 
+if   __name__ == "__main__":
+    app.run()
