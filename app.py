6898e05aa26ffe45d7e5441b62caa9dbcc1fec70
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-08 19:06:39 +0200

    com1

--- app.py
+++ app.py
@@ -14,20 +14,6 @@ gas_data = pd.DataFrame({'Month': ['january','february','march','april','may','j
 water_data = pd.DataFrame({'Month': ['january','february','march','april','may','june','july','august','september','october','november','december'],
                            'Consumption': [75, 75, 77, 78, 77, 80, 82, 85, 82, 81, 86, 75]})
 
-# Function to generate random daily consumption data for each month
-def generate_daily_consumption(months, mean_consumption, std_dev):
-    data = {}
-    for month in months:
-        year = 2023
-        num_days = monthrange(year, list(calendar.month_name).index(month.capitalize()))[1]  # Get the number of days in the month
-        data[month] = np.random.normal(mean_consumption, std_dev, num_days).astype(int)
-    return data
-
-# Calculate daily consumption
-electricity_daily = {key: value.tolist() for key, value in generate_daily_consumption(electricity_data['Month'], mean_consumption=4000, std_dev=1000).items()}
-gas_daily = {key: value.tolist() for key, value in generate_daily_consumption(gas_data['Month'], mean_consumption=2000, std_dev=500).items()}
-water_daily = {key: value.tolist() for key, value in generate_daily_consumption(water_data['Month'], mean_consumption=50, std_dev=10).items()}
-
 
 
 
